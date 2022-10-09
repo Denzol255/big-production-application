ab50ea9a1687b0a16224d37c380debd7f25dfb68
Denzol255 <den-zoloto@mail.ru>
2022-10-09 22:14:19 +0300

    fix ui tests

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,8 +2,6 @@
 import { useTranslation } from 'react-i18next';
 import Logo from 'shared/assets/icons/logo.svg';
 import { getClassNames } from 'shared/lib/getClassNames/getClassNames';
-import { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { AppLink } from 'shared/ui/index';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
