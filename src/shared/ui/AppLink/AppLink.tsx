76ccd3312a4ba131a62c5128caff6d0b637d9335
Denzol255 <den-zoloto@mail.ru>
2022-10-09 14:34:15 +0300

    fix applink

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 import { getClassNames } from 'shared/lib/getClassNames/getClassNames';
-import styles from './Applink.module.scss';
+import styles from './AppLink.module.scss';
 
 export const enum AppLinkTheme {
   PRIMARY = 'primary',
